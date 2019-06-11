23b43bd0c1830fc7a8daa81622bdde5efbd3ba02
lixiaoxi <lixiaoxi@xpsd.com>
2019-06-11 15:22:14 +0800

    agent: 添加阿里arthas other: 更新README.md文件

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 项目|描述
 ---|---
 agent|代理相关技术
+jmeter|JMeter相关代码
+message_broker|消息中间件相关技术(进行中)
+netty|Netty相关技术(暂时坑了)
 
---
+++ agent/src/main/java/com/heaven/elegy/study/agent/arthas/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 阿里的Arthas工具套件
+ * <p><a>https://github.com/alibaba/arthas</a></p>
+ * @author li.xiaoxi
+ * @date 2019/06/11 15:18
+ */
+package com.heaven.elegy.study.agent.arthas;
